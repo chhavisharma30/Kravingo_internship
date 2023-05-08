4facbdcd48048cce2f6e6efdd229d1099f31800b
Chhavi <chhavisharma3007@gmail.com>
2023-05-08 22:34:49 +0530

    Modified action bar, status bar and floating action button

--- app/src/main/java/com/example/datingapp/MainActivity.kt
+++ app/src/main/java/com/example/datingapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.datingapp
 
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+
+        val windowInsetsController=ViewCompat.getWindowInsetsController(window.decorView)
+
+        windowInsetsController?.isAppearanceLightStatusBars=true
     }
 }

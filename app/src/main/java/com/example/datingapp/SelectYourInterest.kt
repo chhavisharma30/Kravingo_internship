0cca760616b313a53108205daf33bb406abe1864
Chhavi <chhavisharma3007@gmail.com>
2023-05-22 23:47:16 +0530

    prepared basic layout of Select Your Interest section

---
+++ app/src/main/java/com/example/datingapp/SelectYourInterest.kt
@@ -0,0 +1,11 @@
+package com.example.datingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectYourInterest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_your_interest)
+    }
+}

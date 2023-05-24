7d31cdc48860d576b173b0f22345f6604f8d6240
Chhavi <chhavisharma3007@gmail.com>
2023-05-24 23:00:14 +0530

    Customized chipgroup and added emojis

--- app/src/main/java/com/example/datingapp/SelectYourInterest.kt
+++ app/src/main/java/com/example/datingapp/SelectYourInterest.kt
@@ -2,10 +2,14 @@ package com.example.datingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.activity_select_your_interest.*
+
 
 class SelectYourInterest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_your_interest)
+
     }
 }

afbbab1c083c7362680e1253dc0d5ea4b2aaad98
Rigved Desai <rigveddesai@gmail.com>
2023-06-07 14:21:05 +0530

    Updated inbox section: Added request adapter to populate data in message requests recyclerView

--- app/src/main/java/com/example/datingapp/Inbox.kt
+++ app/src/main/java/com/example/datingapp/Inbox.kt
@@ -6,17 +6,28 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_inbox.*
 
 class Inbox : AppCompatActivity() {
-    private lateinit var profileAdapter: ProfileAdapter
+    private lateinit var inboxAdapter: InboxAdapter
+    private lateinit var requestAdapter: InboxRequestAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inbox)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView2.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
-        // Assuming you have a list of profile data
-        //val profiles: List<ContactsContract.Profile> = getProfiles()
+//         Assuming you have a list of profile data (Using fake data for now)
+//            matches = getMatches()
+        val matches = ArrayList<MatchModel>()
+        matches.add(MatchModel("User 1", "1234"))
+        matches.add(MatchModel("User 2", "6789"))
+        matches.add(MatchModel("User 3", "0000"))
 
-        //profileAdapter = ProfileAdapter(profiles)
-        //recyclerView2.adapter = profileAdapter
+        inboxAdapter = InboxAdapter(matches, this)
+        recyclerView.adapter = inboxAdapter
+
+        // ** Passing the same list as matches for now however, requestList will have a different getRequests() call and hence a
+        // different list, might also need to create a new data model for requests instead of using matchModel ** //
+        requestAdapter = InboxRequestAdapter(matches, this)
+        recyclerView2.adapter = requestAdapter
     }
 }

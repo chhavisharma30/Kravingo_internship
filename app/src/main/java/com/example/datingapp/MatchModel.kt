f03c5964cbafeb973af33cc6720a8f94477ec222
Rigved Desai <rigveddesai@gmail.com>
2023-07-09 13:35:54 +0530

    Removed redundant files

--- app/src/main/java/com/example/datingapp/MatchModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.datingapp
-
-data class MatchModel (
-    val userName: String,
-    val userID: String,
-    val userProfilePicture: Int? = null // change this to URL when using Firebase
-)
-
